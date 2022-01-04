11adf69a797e53a8a2b56e81f006983bae81e1f1
ianbrinkerhoff09 <ian.brinkerhoff@gmail.com>
2022-01-04 18:42:47 -0500

    changed 127.0.0.1 to localhost in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 	useNewUrlParser: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true,
